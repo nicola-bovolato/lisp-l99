e1414c618c72e4e3473695d456e46b9613134e22
Nicola Bovolato <nicola.bovolato@gmail.com>
2020-11-11 18:01:41 +0100

    renamed functions

--- working-with-lists/p11.lisp
+++ working-with-lists/p11.lisp
@@ -7,16 +7,16 @@
 (load (merge-pathnames "p08.lisp" *load-truename*)) ; compress-list
 (load (merge-pathnames "p09.lisp" *load-truename*)) ; pack-list
 
-(defun encode-list (list)
+(defun encode-list-improved (list)
     (cond
         ((equal list nil) nil)
-        (t (reverse-list (encode-list-fun (pack-list list) '())))
+        (t (reverse-list (encode-list-improved-fun (pack-list list) '())))
     ))
 
-(defun encode-list-fun (list encoded)
+(defun encode-list-improved-fun (list encoded)
     (cond
         ((equal list nil) encoded)
-        (t (encode-list-fun (cdr list) (cons (encode-compress-list (car list)) encoded)))
+        (t (encode-list-improved-fun (cdr list) (cons (encode-compress-list (car list)) encoded)))
     ))
 
 (defun encode-compress-list (list)
@@ -26,9 +26,9 @@
         (t (cons (element-number list) (compress-list list)))
     ))
 
-(assert (equal (encode-list '()) '()))
-(assert (equal (encode-list '(A A A)) '((3 A))))
-(assert (equal (encode-list '(A B C D)) '(A B C D)))
-(assert (equal (encode-list '(A A B A A C D)) '((2 A) B (2 A) C D)))
-(assert (equal (encode-list '(A B C D D D D)) '(A B C (4 D))))
-(assert (equal (encode-list '(A A A B B B B B C C D C)) '((3 A) (5 B) (2 C) D C)))
+(assert (equal (encode-list-improved '()) '()))
+(assert (equal (encode-list-improved '(A A A)) '((3 A))))
+(assert (equal (encode-list-improved '(A B C D)) '(A B C D)))
+(assert (equal (encode-list-improved '(A A B A A C D)) '((2 A) B (2 A) C D)))
+(assert (equal (encode-list-improved '(A B C D D D D)) '(A B C (4 D))))
+(assert (equal (encode-list-improved '(A A A B B B B B C C D C)) '((3 A) (5 B) (2 C) D C)))
--- working-with-lists/p13.lisp
+++ working-with-lists/p13.lisp
@@ -5,31 +5,31 @@
 
 (load (merge-pathnames "p05.lisp" *load-truename*)) ; reverse-list
 
-(defun encode-list (list)
+(defun encode-list-improved-alt (list)
     (cond
         ((equal list nil) nil)
-        (t (reverse-list (encode-list-fun list '() nil 0)))
+        (t (reverse-list (encode-list-improved-alt-fun list '() nil 0)))
     ))
 
-(defun encode-list-fun (list encoded element count)
+(defun encode-list-improved-alt-fun (list encoded element count)
     (cond
         ((equal list nil) (cond
                 ((= count 1) (cons element encoded))
                 (t (cons (cons count (cons element nil)) encoded))
             ))
-        ((equal element (car list)) (encode-list-fun (cdr list) encoded element (+ count 1)))
+        ((equal element (car list)) (encode-list-improved-alt-fun (cdr list) encoded element (+ count 1)))
         (t 
             (cond
-                ((equal element nil) (encode-list-fun list encoded (car list) 0))
-                ((= count 1) (encode-list-fun list (cons element encoded) nil 0))
-                (t (encode-list-fun list (cons (cons count (cons element nil)) encoded) nil 0))
+                ((equal element nil) (encode-list-improved-alt-fun list encoded (car list) 0))
+                ((= count 1) (encode-list-improved-alt-fun list (cons element encoded) nil 0))
+                (t (encode-list-improved-alt-fun list (cons (cons count (cons element nil)) encoded) nil 0))
             )
         ) 
     ))
 
-(assert (equal (encode-list '()) '()))
-(assert (equal (encode-list '(A A A)) '((3 A))))
-(assert (equal (encode-list '(A B C D)) '(A B C D)))
-(assert (equal (encode-list '(A A B A A C D)) '((2 A) B (2 A) C D)))
-(assert (equal (encode-list '(A B C D D D D)) '(A B C (4 D))))
-(assert (equal (encode-list '(A A A B B B B B C C D C)) '((3 A) (5 B) (2 C) D C)))
+(assert (equal (encode-list-improved-alt '()) '()))
+(assert (equal (encode-list-improved-alt '(A A A)) '((3 A))))
+(assert (equal (encode-list-improved-alt '(A B C D)) '(A B C D)))
+(assert (equal (encode-list-improved-alt '(A A B A A C D)) '((2 A) B (2 A) C D)))
+(assert (equal (encode-list-improved-alt '(A B C D D D D)) '(A B C (4 D))))
+(assert (equal (encode-list-improved-alt '(A A A B B B B B C C D C)) '((3 A) (5 B) (2 C) D C)))
